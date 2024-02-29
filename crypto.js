5d9a98d80774a7cc83bb4f2ff7593539f096850e
Fernando Avendaño Hernandez <fernando.softdesigner@gmail.com>
2024-02-29 10:26:56 -0600

    modifique los botones para que fueran independientes y omiti el boton de tranformar y agregue estilos

--- crypto.js
+++ crypto.js
@@ -1,36 +1,34 @@
+function encriptar() {
+  var texto = document.getElementById('texto').value.toLowerCase();
+  var resultado = encriptarTexto(texto);
+  document.getElementById('resultado').value = resultado;
+}
 
+function desencriptar() {
+  var texto = document.getElementById('texto').value.toLowerCase();
+  var resultado = desencriptarTexto(texto);
+  document.getElementById('resultado').value = resultado;
+}
 
-function transformar() {
-    var texto = document.getElementById('texto').value.toLowerCase();
-    var tipo = document.getElementById('tipo').value;
-    var resultado = '';
-    if (tipo === 'encriptar') {
-      resultado = encriptarTexto(texto);
-    } else {
-      resultado = desencriptarTexto(texto);
-    }
-    document.getElementById('resultado').value = resultado;
-  }
+function encriptarTexto(texto) {
+  return texto.replace(/e/g, 'enter')
+             .replace(/i/g, 'imes')
+             .replace(/a/g, 'ai')
+             .replace(/o/g, 'ober')
+             .replace(/u/g, 'ufat');
+}
 
-  function encriptarTexto(texto) {
-    return texto.replace(/e/g, 'enter')
-               .replace(/i/g, 'imes')
-               .replace(/a/g, 'ai')
-               .replace(/o/g, 'ober')
-               .replace(/u/g, 'ufat');
-  }
+function desencriptarTexto(texto) {
+  return texto.replace(/enter/g, 'e')
+             .replace(/imes/g, 'i')
+             .replace(/ai/g, 'a')
+             .replace(/ober/g, 'o')
+             .replace(/ufat/g, 'u');
+}
 
-  function desencriptarTexto(texto) {
-    return texto.replace(/enter/g, 'e')
-               .replace(/imes/g, 'i')
-               .replace(/ai/g, 'a')
-               .replace(/ober/g, 'o')
-               .replace(/ufat/g, 'u');
-  }
-
-  function copiar() {
-    var resultado = document.getElementById('resultado');
-    resultado.select();
-    document.execCommand('copy');
-    alert('Texto copiado al portapapeles');
-  }
+function copiar() {
+  var resultado = document.getElementById('resultado');
+  resultado.select();
+  document.execCommand('copy');
+  alert('Texto copiado al portapapeles');
+}
